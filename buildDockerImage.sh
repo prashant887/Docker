96c510019481c82b288b18d2b1adaf2f02300516
pl465j <pl465j@att.com>
2021-07-31 22:04:11 +0530

    added persistant volume

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -3,3 +3,5 @@ docker build . -t prashant887/firstimage:1.0
 ## /Users/pl465j/Documents/Docker
 
 docker push prashant887/firstimage:1.01
+
+docker run -it  -v /tmp/docker_volume:/test_dir prashant887/firstimage:1.0 /bin/bash
