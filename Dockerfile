6ec4343946e84918195230994dc5445027853512
pl465j <pl465j@att.com>
2021-08-03 21:05:08 +0530

    VAR and ENV Added

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,11 @@ FROM ubuntu
 
 MAINTAINER Prashant L <prashant887@gmail.com> 
 
+ARG MYVAR="DEF"
 RUN apt-get update 
 RUN mkdir /test_dir 
 RUN echo "Hi Hello" > /test_dir/first_file.txt
-ENV MYVAR "DEF" 
+ENV MYVAR=$MYVAR 
 RUN echo $MYVAR >> /test_dir/first_file.txt
 ADD test_cript.sh /test_dir
 
--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,3 +5,8 @@ docker build . -t prashant887/firstimage:1.0
 docker push prashant887/firstimage:1.01
 
 docker run -it  -v /tmp/docker_volume:/test_dir prashant887/firstimage:1.0 /bin/bash
+
+
+docker build . -t prashant887/firstimage:1.0  --build-arg MYVAR="OldVal"
+
+docker run -it -e MYVAR="Prashant"  prashant887/firstimage:1.0 /bin/bash
